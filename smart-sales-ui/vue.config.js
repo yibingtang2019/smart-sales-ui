716b9e5c7241e40acfa1bc698187c1838a03b743
Daniel Tang <yibing.tang@163.com>
2021-03-03 20:15:31 +0800

    change ports

--- smart-sales-ui/vue.config.js
+++ smart-sales-ui/vue.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
     devServer: {
-        port: 8080,
+        port: 8082,
         overlay: {
             warnings: false,
             errors: false
